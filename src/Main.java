6e6a153c5b27c7ac892bb3e78b8d04a4df1c6ea4
rushi <rushikeshsangale57@gmail.com>
2024-08-25 18:43:13 +0530

    Account Class is Created

--- src/Main.java
+++ src/Main.java
@@ -3,3 +3,13 @@ public class Main {
         System.out.println("Debit Account Program");
     }
 }
+class Account {
+    private double balance;
+
+    // Constructor to initialize the account with a balance
+    public Account(double initialBalance) {
+        if (initialBalance > 0.0) {
+            this.balance = initialBalance;
+        }
+    }
+}
